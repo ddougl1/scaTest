593c6dd7fdd5297d9da3f8a35ca336ec685bf01b
Dave Douglas <dave.douglas@nuix.com>
2020-10-12 12:20:14 -0400

    a

---
+++ scaTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+namespace scaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
